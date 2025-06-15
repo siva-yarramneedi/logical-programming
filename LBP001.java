968584d948fb06b1e1ac29af52994e50ae0ad504
Siva Yarramneedi <yarramneedisiva143@gmail.com>
2025-06-15 10:11:34 +0530

    used ternary operator

--- LBP001.java
+++ LBP001.java
@@ -1,10 +1,7 @@
 import java.util.Scanner;
 public class LBP001{
     public static String isEvenOrOdd(int value){
-            if(value<0){
-                return "invalid";
-            }
-            return value%2==0?"even":"odd";
+            return value<0?"invalid":(value%2==0?"even":"odd");
     }
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
